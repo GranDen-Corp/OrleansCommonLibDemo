216d86419be7d1fb51451f5ece67e5e83650d227
Chen Yu Pao <windperson@gmail.com>
2019-09-16 17:17:12 +0800

    add help text for PowerShell

--- deploy_conf/deploy.ps1
+++ deploy_conf/deploy.ps1
@@ -1,30 +1,73 @@
 #!/usr/bin/env pwsh
+
+<#
+.SYNOPSIS
+
+Scripts to deploy docker-compose configurated combination services.
+
+.DESCRIPTION
+
+This PowerShell script combined with the docker-compose.yml files to pull and deploy docker images from Docker Registry.
+
+.EXAMPLE
+./deploy.ps1 -registry [The_remote_registry] pull
+# Docker pull all required images.
+
+.EXAMPLE 
+./deploy.ps1 -registry [The_remote_registry] -https_cert_pass [SSL_certificate_file_password] --project-name [Custom_project_name] up --detach
+# Run whole docker compose project.
+
+#>
+
 # input parameter
 [CmdletBinding(PositionalBinding = $false)]
 param(
     [Parameter(HelpMessage = "docker registry")]
-    [String]$registry,
+    [String]
+    #Specify the docker registry that will push to.
+    $registry,
     [Parameter(HelpMessage = "silo host version")]
-    [String]$siloHostVer,
+    [String]
+    #Silo Host version number.
+    $siloHostVer,
     [Parameter(HelpMessage = "web client version")]
-    [String]$webClientVer,
+    [String]
+    #Invoke Orleans RPC web client version number.
+    $webClientVer,
     [Parameter(HelpMessage = "Helloworld grain version")]
-    [String]$helloworld_grain_ver,
+    [String]
+    #Helloworld grain version number.
+    $helloworld_grain_ver,
     [Parameter(HelpMessage = "MyReminder grain version")]
-    [String]$myreminder_grain_ver,
+    [String]
+    #MyReminder grain version number.
+    $myreminder_grain_ver,
     [Parameter(HelpMessage = "NumberGenerator grain version")]
-    [String]$numbergenerator_grain_ver,
+    [String]
+    #Number generator version number.
+    $numbergenerator_grain_ver,
     [Parameter(HelpMessage = "Override all grains version")]
-    [String]$override_grains_ver,
+    [String]
+    #Specify all Orleans Grain docker images version tag that docker-compose use.
+    $override_grains_ver,
     [Parameter(HelpMessage = "Override docker image version")]
-    [String]$override_img_ver,
+    [String]
+    #Specify all docker images version tag that docker-compose use.
+    $override_img_ver,
     [Parameter(HelpMessage = "HTTP Port")]
-    [int]$http_port,
+    [int]
+    #Web client Http port.
+    $http_port,
     [Parameter(HelpMessage = "HTTPS Port")]
-    [int]$https_port,
+    [int]
+    #Web client Https port.
+    $https_port,
     [Parameter(HelpMessage = "SSL cert password")]
-    [String]$https_cert_pass,
+    [String]
+    #Web client Https SSL certificate password.
+    $https_cert_pass,
     [Parameter(ValueFromRemainingArguments)]
+    #Other command line arguments for docker-compose, needs to use full parameter syntax.
     [String]$other_args
 )
 

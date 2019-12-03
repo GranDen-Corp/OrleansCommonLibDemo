a3d1616dab38124d90f2291ac2aa75b8d3acfa9f
Chen Yu Pao <windperson@gmail.com>
2019-12-03 12:41:12 +0800

    Use new way to do persistence

--- ServerSide/Grains/HelloWorld.Grain/Tracker/VisitTracker.cs
+++ ServerSide/Grains/HelloWorld.Grain/Tracker/VisitTracker.cs
@@ -1,37 +1,40 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Orleans;
+using Orleans.Runtime;
 using VisitTracker.ShareInterface;
 
 namespace HelloWorld.Grain.Tracker
 {
-    public class VisitTracker : Grain<VisitTrackerState>, IVisitTracker
+    public class VisitTracker : Orleans.Grain, IVisitTracker
     {
         private readonly ILogger<VisitTracker> _logger;
+        private readonly IPersistentState<VisitTrackerState> _visitTrackerState;
 
-        public VisitTracker(ILogger<VisitTracker> logger)
+        public VisitTracker(ILogger<VisitTracker> logger,
+            [PersistentState(nameof(VisitTracker))]IPersistentState<VisitTrackerState> visitTrackerState)
         {
             _logger = logger;
+            _visitTrackerState = visitTrackerState;
         }
 
         public Task<int> GetNumberOfVisits()
         {
-            return Task.FromResult(State.NumberOfVisits);
+            return Task.FromResult(_visitTrackerState.State.NumberOfVisits);
         }
 
         public async Task VisitAsync()
         {
             var now = DateTime.Now;
-            if (!State.FirstVisit.HasValue)
+            if (!_visitTrackerState.State.FirstVisit.HasValue)
             {
-                State.FirstVisit = now;
+                _visitTrackerState.State.FirstVisit = now;
             }
 
-            State.NumberOfVisits++;
-            State.LastVisit = now;
+            _visitTrackerState.State.NumberOfVisits++;
+            _visitTrackerState.State.LastVisit = now;
             _logger.LogInformation($"Visit at {now}");
-            await WriteStateAsync();
+            await _visitTrackerState.WriteStateAsync();
         }
     }
 }
